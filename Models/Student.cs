59fdb3d9e346f8a694780242a04a808f4b5b876d
Eagma <mehdigolipoor@outlook.com>
2023-02-15 17:58:41 -0500

    LAB3

---
+++ Models/Student.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Student
+    {
+        [Required]
+        [BindProperty(Name = "FirstName")]
+        public string FirstName {
+            get;
+            set;
+        }
+
+
+        [Required]
+        [BindProperty(Name = "lastName")]
+        public string LastName {
+            get;
+            set;
+        }
+
+        [Required]
+        [EmailAddress]
+        [BindProperty(Name = "email")]
+        public string EmailAddress {
+            get;
+            set;
+        }
+
+        [Required]
+        [BindProperty(Name = "id")]
+        public string StudentId {
+            get;
+            set;
+        }
+
+        [Required]
+        [BindProperty(Name = "description")]
+        public string Description {
+            get;
+            set;
+        }
+    }
+}
